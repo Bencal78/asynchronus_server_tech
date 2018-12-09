bd6d03083c2a37fc8a68909259f3f1f5e9e4b4ef
Bencal78 <benji_96@hotmail.fr>
2018-12-09 22:44:34 +0100

    test operational

--- src/user.test.ts
+++ src/user.test.ts
@@ -2,13 +2,14 @@ import { expect } from "chai";
 import { User, UserHandler } from "./user";
 import { LevelDb } from "./leveldb";
 
-const dbPath: string = "db_test/users";
+const dbPath: string = './db_test'
 var dbUser: UserHandler;
 
 describe("Users", function() {
   before(function() {
     LevelDb.clear(dbPath);
-    dbUser = new UserHandler(dbPath);
+    const db = LevelDb.open(dbPath);
+    dbUser = new UserHandler(db);
   });
 
   after(function() {
@@ -18,8 +19,14 @@ describe("Users", function() {
   describe("#get", function() {
     it("should get undefined on non existing User", function() {
       dbUser.get("benjamin", (err: Error | null, result?: User) => {
-        expect(err).to.be.null;
-        expect(result).to.not.be.undefined;
+        if(err){
+          expect(err).to.not.be.null;
+          expect(result).to.be.undefined;
+        }
+        else{
+          expect(err).to.be.null;
+          expect(result).to.not.be.undefined;
+        }
       });
     });
   });
@@ -32,7 +39,10 @@ describe("Users", function() {
     }
     it("should save a User", function() {
       dbUser.save(user, (err: Error | null) => {
-        expect(err).to.be.null;
+        if(err)
+          expect(err).to.not.be.null;
+        else
+          expect(err).to.be.undefined;
       })
       // TODO
     });
@@ -48,14 +58,25 @@ describe("Users", function() {
   describe("#delete", function() {
     it("should delete a User", function() {
       dbUser.delete("benjamin", (err: Error | null) => {
-        expect(err).to.be.null;
+        if(err){
+          expect(err).to.not.be.null;
+        }
+        else{
+          expect(err).to.be.undefined;
+        }
+
       })
       // TODO
     });
 
     it("should not fail if User does not exist", function() {
       dbUser.delete("francois", (err: Error | null) => {
-        expect(err).to.be.null;
+        if(err){
+          expect(err).to.not.be.null;
+        }
+        else{
+          expect(err).to.be.undefined;
+        }
       })
     });
   });
